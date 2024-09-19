db98c8f417833acc50e83a955207436e7a3495e0
Ismael Younes <2023_Ismael.younes@iticbcn.cat>
2024-09-19 19:09:04 +0200

    treballa a clase del dia 19/09/24

---
+++ rot13.java
@@ -0,0 +1,24 @@
+public class rot13 {
+    public static final char[] lMin = { 'a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l', 'm',
+            'n', 'ñ', 'o', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'v', 'x', 'y', 'z' };
+
+    public static final char[] lMay = { 'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M',
+            'N', 'Ñ', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'V', 'X', 'Y', 'Z' };
+
+    public Void xifraRot13(String cadena) {
+
+    }
+
+    public static void main(String[] args) {
+        String cadena = Entrada.readLine();
+        if (cadena.isBlank()) {
+            return;
+        }
+        for(int lCa=0; l< cadena.length();l++){
+            for(int lCh=){
+
+            }
+        }
+
+    }
+}
