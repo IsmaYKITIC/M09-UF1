effabd599d7456227e9132a165f2f0fe0017d930
Ismael Younes <2023_Ismael.younes@iticbcn.cat>
2024-09-25 18:22:26 +0200

    exercici finalitzat

--- rot13.java
+++ rot13.java
@@ -19,36 +19,105 @@ public class rot13 {
             } else {
                 esMajuscula = false;
             }
+
             if (esMajuscula) {
                 for (int c = 0; c < lMay.length; c++) {
                     if (cadena.charAt(l) == lMay[c]) {
-                        posicio = ((posicio + 13) - lMay.length);
+                        posicio = c + 13;
+                        if (posicio >= lMay.length) {
+                            posicio = posicio - lMay.length;
+                        }
                         nCadena = nCadena + lMay[posicio];
+                        break;
                     }
-                    posicio = 0;
                 }
             } else {
                 for (int c = 0; c < lMin.length; c++) {
-                    if (cadena.charAt(l) == lMay[c]) {
-                        posicio = ((posicio + 13) - lMin.length);
+                    if (cadena.charAt(l) == lMin[c]) {
+                        posicio = c + 13;
+                        if (posicio >= lMin.length) {
+                            posicio = posicio - lMin.length;
+                        }
                         nCadena = nCadena + lMin[posicio];
+                        break;
                     }
-                    posicio = 0;
                 }
             }
-
         }
         return nCadena;
     }
 
+    public static String desxifraRot13(String cadena) {
+        // L= lletra C= Caracter
+        String nCadena = "";
+        boolean esMajuscula;
+        int posicio = 0;
+
+        for (int l = 0; l < cadena.length(); l++) {
+            if (Character.isUpperCase(cadena.charAt(l))) {
+                esMajuscula = true;
+            } else {
+                esMajuscula = false;
+            }
+
+            if (esMajuscula) {
+                for (int c = 0; c < lMay.length; c++) {
+                    if (cadena.charAt(l) == lMay[c]) {
+                        posicio = c + -13;
+                        if (posicio < 0) {
+                            posicio = posicio + lMay.length;
+                        }
+                        nCadena = nCadena + lMay[posicio];
+                        break;
+                    }
+                }
+            } else {
+                for (int c = 0; c < lMin.length; c++) {
+                    if (cadena.charAt(l) == lMin[c]) {
+                        posicio = c + -13;
+                        if (posicio < 0) {
+                            posicio = posicio + lMin.length;
+                        }
+                        nCadena = nCadena + lMin[posicio];
+                        break;
+                    }
+                }
+            }
+        }
+        return nCadena;
+    }
+
+    public static String confirmador() {
+        Scanner scanner = new Scanner(System.in);
+        String text;
+        System.out.println("Vols xifra o desxifra?");
+        while (true) {
+            text = scanner.nextLine();
+            if (text.equals("xifra") || text.equals("Xifra") || text.equals("XIFRA")) {
+                return text.toLowerCase();
+            } else if (text.equals("desxifra") || text.equals("Desxifra") || text.equals("DESXIFRA")) {
+                return text.toLowerCase();
+            } else {
+
+            }
+            System.out.println("No té entès, torna-ho a escriure.");
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingresa un text per codificar.");
+        String funcio = confirmador();
+
+        System.out.println("Ingresa un text per codificar:");
         String cadena = scanner.nextLine();
         if (cadena.isBlank()) {
             return;
         }
-        String Text = xifraRot13(cadena);
-        System.out.println(Text);
+        if (funcio.equals("xifra")) {
+            System.out.println(xifraRot13(cadena));
+        } else {
+            System.out.println(desxifraRot13(cadena));
+        }
     }
 }
