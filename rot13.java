73003f8922e867aa256e60b58cc4458d30bade95
Ismael Younes <2023_Ismael.younes@iticbcn.cat>
2024-09-25 16:06:20 +0200

    actualitzacio del projecte

--- rot13.java
+++ rot13.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class rot13 {
     public static final char[] lMin = { 'a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l', 'm',
             'n', 'ñ', 'o', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'v', 'x', 'y', 'z' };
@@ -5,20 +7,48 @@ public class rot13 {
     public static final char[] lMay = { 'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M',
             'N', 'Ñ', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'V', 'X', 'Y', 'Z' };
 
-    public Void xifraRot13(String cadena) {
+    public static String xifraRot13(String cadena) {
+        // L= lletra C= Caracter
+        String nCadena = "";
+        boolean esMajuscula;
+        int posicio = 0;
 
+        for (int l = 0; l < cadena.length(); l++) {
+            if (Character.isUpperCase(cadena.charAt(l))) {
+                esMajuscula = true;
+            } else {
+                esMajuscula = false;
+            }
+            if (esMajuscula) {
+                for (int c = 0; c < lMay.length; c++) {
+                    if (cadena.charAt(l) == lMay[c]) {
+                        posicio = ((posicio + 13) - lMay.length);
+                        nCadena = nCadena + lMay[posicio];
+                    }
+                    posicio = 0;
+                }
+            } else {
+                for (int c = 0; c < lMin.length; c++) {
+                    if (cadena.charAt(l) == lMay[c]) {
+                        posicio = ((posicio + 13) - lMin.length);
+                        nCadena = nCadena + lMin[posicio];
+                    }
+                    posicio = 0;
+                }
+            }
+
+        }
+        return nCadena;
     }
 
     public static void main(String[] args) {
-        String cadena = Entrada.readLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa un text per codificar.");
+        String cadena = scanner.nextLine();
         if (cadena.isBlank()) {
             return;
         }
-        for(int lCa=0; l< cadena.length();l++){
-            for(int lCh=){
-
-            }
-        }
-
+        String Text = xifraRot13(cadena);
+        System.out.println(Text);
     }
 }
