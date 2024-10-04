bdc6f5bd71cfaa3d89f7a28c93f7f3f1660964ee
Isma <2023_Ismael.younes@iticbcn.cat>
2024-10-04 11:48:23 +0200

    Activitat3 començada 03/10/2024

---
+++ Activitat3/Monoalfabetic.java
@@ -0,0 +1,20 @@
+public class Monoalfabetic {
+    public static final char[] LMAY = {
+        'A', 'Á', 'À', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È', 'F', 'G', 'H', 'I', 'Í', 'Ì', 'Ï', 'J', 'K', 'L', 'M',
+        'N', 'Ñ', 'O', 'Ó', 'Ò', 'Ö', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ù', 'Ü', 'V', 'X', 'Y', 'Z'
+    };
+    public static char[] alfabetNou;
+    public static char[] permutaAlfabet(char[] abecedari){
+
+    }
+    public static String xifraMonoAlfa(String cadena){
+
+    }
+    public static String deixifraMonoAlfa(String cadena){
+
+    }
+    public static void main(String[] args) {
+           
+    }
+    
+}
